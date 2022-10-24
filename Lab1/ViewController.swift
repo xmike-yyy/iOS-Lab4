096cd9f4c84a4537bed2ad88327a66c6ff226b6b
xmike-yyy <michaelsunmaybe@gmail.com>
2022-10-24 17:54:13 -0400

    Add everything

--- Lab1/ViewController.swift
+++ Lab1/ViewController.swift
@@ -43,18 +43,66 @@ class ViewController: UIViewController {
         currentIndex = currentIndex + 1
         updateLabels()
         updateNextPrevButtons()
+        animateCardOut()
     }
     @IBAction func didTapOnPrev(_ sender: Any) {
         currentIndex = currentIndex - 1
         updateLabels()
         updateNextPrevButtons()
+        prevanimateCardOut()
+    }
+    func flipFlashcard(){
+
+        UIView.transition(with: card, duration: 0.3, options: .transitionFlipFromRight, animations: {
+            self.frontLabel.isHidden = true
+        })
+    }
+    func unflipFlashcard(){
+
+        UIView.transition(with: card, duration: 0.3, options: .transitionFlipFromRight, animations: {
+            self.frontLabel.isHidden = false
+        })
+    }
+    func animateCardOut(){
+        UIView.animate(withDuration: 0.3, animations: {
+            self.card.transform = CGAffineTransform.identity.translatedBy(x: -300.0, y: 0.0)
+        }, completion: {finished in
+            self.updateLabels()
+            self.animateCardIn()
+        })
+        
+    }
+    func animateCardIn(){
+        card.transform = CGAffineTransform.identity.translatedBy(x: 300.0, y: 0.0)
+        UIView.animate(withDuration: 0.3){
+            self.card.transform = CGAffineTransform.identity
+        }
+        
+    }
+    func prevanimateCardOut(){
+        UIView.animate(withDuration: 0.3, animations: {
+            self.card.transform = CGAffineTransform.identity.translatedBy(x: 300.0, y: 0.0)
+        }, completion: {finished in
+            self.updateLabels()
+            self.prevanimateCardIn()
+        })
+        
+    }
+    func prevanimateCardIn(){
+        card.transform = CGAffineTransform.identity.translatedBy(x: -300.0, y: 0.0)
+        UIView.animate(withDuration: 0.3){
+            self.card.transform = CGAffineTransform.identity
+        }
+        
     }
     
     @IBAction func didtapFlashcard(_ sender: Any) {
         if frontLabel.isHidden == true {
             frontLabel.isHidden = false
+            unflipFlashcard()
         }
-        else{ frontLabel.isHidden = true
+        else{
+            flipFlashcard()
         }
     }
     func updateFlashcards(question:String, answer: String){
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 📝 `NOTE` Paste this template at the top of your existing `README.md` file from the last lab.
 
-## Lab 3
+## Lab 4
 
 ### App Description
 `TODO://` Add app description
@@ -8,15 +8,18 @@
 ### App Walk-though
 `TODO://` Add the URL to your animated app walk-though `gif` in the image tag below. Make sure the gif actually renders and animates when viewing this README. (☝️ Remove this paragraph after after adding gif)
 
-<img src="three.gif" width=200><br>
+<img src="YOUR_GIF_URL_HERE" width=200><br>
 
 `TODO://` In the User stories section below, add an `x` between the `-[ ]` like this `- [x]` for any user story you complete. (☝️ Remove this paragraph after after checking off user stories)
 
 ## Required
-- [x] User can browse through multiple flashcards
-- [x] User can re-open the app and see previously created flashcards
+- [x] User should see the app icon 
+- [x] User should see a splash screen
+- [x] User should see an flip animation when revealing the answer
+- [x] User should see a slide animation when going to the next flashcard
+- [x] User should see a slide animation when going to the previous flashcard
 - [x] Push code to GitHub
 ## Optional
-- [ ] User can delete a flashcard
-- [ ] User can edit existing flashcard
-- [ ] User can store multiple choice questions
+- [ ] User should see a nicer flaschard with rounded corners, shadows, and animating when showing the first time
+- [ ] User should see all views properly layout when using the app on a phone of different sizes
+- [ ] Correct answer in multiple choice should be random
